856fd016e548083f79a83cb7e183f60985899151
hobbang <hocheol0303@gmail.com>
2024-08-28 09:15:57 +0900

    9019_L,R,string

--- algo.py
+++ algo.py
@@ -6,16 +6,19 @@ R: append(popleft)
 
 
 컨닝했숴: 다이나믹 프로그래밍으로 탈출조건 visited[target] == 1인 반복문
+L, R 4자리수 훼이크 개쉐이야
+문자열 += 문제????제발요
 '''
 import sys
 from collections import deque
 
 def bfs(x, target):
-    global visited, result
+    global visited, result, start
     # 방문한적 있으면 나가
     if visited[target] == 1:
         return
     q = deque()
+    visited[x] = 1
     q.append(x)
 
     # target에 방문할 때까지 찾아가
@@ -30,7 +33,8 @@ def bfs(x, target):
             if x == 0:
                 pass
             else:
-                result[calc] = tmp
+                if calc != start:
+                    result[calc] = tmp  
                 q.append(calc)
                 visited[calc]=1
         
@@ -38,7 +42,8 @@ def bfs(x, target):
         calc=S(x)
         length=len(result[calc])
         if len(tmp) < length or length==0:
-            result[calc] = tmp
+            if calc != start:
+                result[calc] = tmp
             q.append(calc)
             visited[calc]=1
         
@@ -46,7 +51,8 @@ def bfs(x, target):
         calc=L(x)
         length=len(result[calc])
         if len(tmp) < length or length==0:
-            result[calc] = tmp
+            if calc != start:
+                result[calc] = tmp
             q.append(calc)
             visited[calc]=1
         
@@ -54,7 +60,8 @@ def bfs(x, target):
         calc=R(x)
         length=len(result[calc])
         if len(tmp) < length or length==0:
-            result[calc] = tmp
+            if calc != start:
+                result[calc] = tmp
             q.append(calc)
             visited[calc]=1
             
@@ -64,26 +71,30 @@ def D(x):
 def S(x):
     return 9999 if x==0 else x-1
 
+# 앞에서 떼다가 뒤에 붙여: 4자리수 아니면 0 채워: 어디 채워 -> 앞에 채워
 def L(x):
     x = list(str(x))
+    if len(x) !=4:
+        for i in range(4-len(x)):
+            x.insert(0, '0')
     x.append(x.pop(0))
-    result=''
-    for i in x:
-        result+=i
+    result=''.join(x)
     
     return int(result)
 
+# 뒤에서 떼다가 앞에 붙여: 4자리수 아니면 0 채워: 어디 채워 -> 앞에 채워
 def R(x):
     x = list(str(x))
+    if len(x) != 4:
+        for i in range(4-len(x)):
+            x.insert(0,'0')
     x.insert(0, x.pop())
 
-    result=''
-    for i in x:
-        result+=i
+    result=''.join(x)
     return int(result)
 
-# print(R(1234))
-# print(L(1234))
+# print(R(407))
+# print(L(407))
 # print(D(4000), D(5000))
 # print(S(1), S(2))
 
@@ -94,7 +105,7 @@ n = int(sys.stdin.readline())
 for _ in range(n):
     visited = [0] * 10001
     result = ['']*10001
-    x,target=map(int,sys.stdin.readline().split())
-    bfs(x, target)
+    start,target=map(int,sys.stdin.readline().split())
+    bfs(start, target)
 
     print(result[target])
