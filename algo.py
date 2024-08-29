4bd653bc177039416bbfdb313d4daa38070ce2f3
hobbang <hocheol0303@gmail.com>
2024-08-29 09:04:32 +0900

    17626_middle_save

--- algo.py
+++ algo.py
@@ -8,21 +8,21 @@
 최대로 놓고 min()으로 쭉쭉쭉
 '''
 import sys
+# import time
 
 lst= [4] * 50001
 i=1
 n=int(sys.stdin.readline())
-
+# start=time.time()
 lst[1]=1
 lst[2]=2
 lst[3]=3
 
 # 26 : [5, 1]={25,1} / [4, 3, 1]={4^2,10}
-
-while i < n:
+while i <= n:
     for j in range(1, i):
-        # if (i==25 and j==1) or (i==5 and j==1) :
-        #     print()
+        if i==22500 and j==5032:
+            print()
 
         if i**2+j**2 <= n:
             lst[i**2+j**2]=2
@@ -30,12 +30,10 @@ while i < n:
             lst[i**2]=1
         if j**2 <= n:
             lst[j**2]=1
-            
-        # lst[j**2 + i] += 1
-        # lst[i], j**2
-        # lst[i+j**2]을 lst[i]+1
         if i+j**2 <= n:
             lst[i+j**2] = min(lst[i+j**2], lst[i]+1)
     i+=1
 
 print(lst[n])
+# end=time.time()
+# print(end-start)
