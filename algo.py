1a1b1786a91fad250400c2971aa2bed80f1ad9d0
hobbang <hocheol0303@gmail.com>
2024-08-30 21:04:52 +0900

    2630_color_paper

--- algo.py
+++ algo.py
@@ -1,30 +1,61 @@
 '''
-다이나믹 프로그래밍 문제
-퍼져나가면서 그자리에 적는거여
-
-
-
-"넷, 혹은 그 이하"
-최대로 놓고 min()으로 쭉쭉쭉
-
-답: 현재 들고 있는 값 i에 대해서 {i - (뭐뭐의 제곱)}의 제곱수 개수에 (뭐뭐의 제곱 하나)를 더한거
-
-먼저번에 내가 풀었던거는 범위가 너무 넓었던 것. 3000의 제곱도 막 구하고 한거같음
+전체 종이가 모두 같은 색이 아니면 가로세로 반띵해서 4군데로 나눠 다시 탐색
+끝까지 탐색하면 모든 색이 같은색인거로
 '''
+
+def func(row_start, row_end, col_start, col_end):
+    global ones, zeros, lst
+    notation=lst[row_start][col_start]
+
+    #탈출조건: 원소 하나짜리 넘어왔을 때 값 올리고 탈출
+    if row_start == row_end and col_start == col_end:
+        if notation == 1:
+            ones+=1
+            return
+        else:
+            zeros+=1
+            return
+    
+    # 다른거 적발되면 4개로 나눠서 재귀
+    for r in range(row_start, row_end):
+        for c in range(col_start, col_end):
+            if lst[r][c] != notation:
+                r = (row_start+row_end) // 2
+                c = (col_start+col_end) // 2
+                
+                func(row_start, r, col_start, c)
+                func(row_start, r, c, col_end)
+
+                func(r, row_end, col_start, c)
+                func(r, row_end, c, col_end)
+                return
+            
+    # 적발 안됐으면 클-린 -> 값 올려
+    if notation == 1:
+        ones+=1
+    else:
+        zeros+=1
+
+
 import sys
 
 n=int(sys.stdin.readline())
+lst=[]
+ones=0
+zeros=0
+for i in range(n):
+    lst.append(list(map(int, sys.stdin.readline().split())))
 
-lst= [1 if ((i**0.5)*10)%10==0 else 4 for i in range(50001)]
+# for i in lst:
+#     print(i)
+row = col = n
 
-lst[1]=1
-lst[2]=2
-lst[3]=3
+check=lst[0][0]
 
-for i in range(4, n+1):
-    j=1
-    while(j**2) <= i:
-        lst[i] = min(lst[i], lst[i-j**2]+1)
-        j+=1
+row_start=col_start=0
+row_end=col_end=n
 
-print(lst[n])
+func(row_start, row_end, col_start, col_end)
+
+print(zeros)
+print(ones)
