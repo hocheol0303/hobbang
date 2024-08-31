30795cdae0def8b73d4ddc8c831161b943e5b479
hobbang <hocheol0303@gmail.com>
2024-08-31 10:06:26 +0900

    1697_what_is_the_matter?????

--- algo.py
+++ algo.py
@@ -7,43 +7,32 @@ import sys
 from collections import deque
 
 def bfs(x, y):
-    global lst, cnt
+    global lst
     # dx = +1, -1, *2
     q = deque()
     q.append(x)
 
     while q:
         x=q.popleft()
-        if x >= 100000 or x < 0:
-            continue
+        # if x >= 100000 or x < 0:
+        #     continue
             
-        if cnt[x+1]<3:
-            cnt[x+1]+=1
-            q.append(x+1)
-            lst[x+1]=min(lst[x+1], lst[x]+1)
+        if x < 100000:
+            if lst[x+1]==0:
+                q.append(x+1)
+                lst[x+1]=lst[x]+1
 
-        if cnt[x-1]<3:
-            cnt[x-1]+=1
+        if x>0 and lst[x-1]==0:
             q.append(x-1)
-            lst[x-1]=min(lst[x-1], lst[x]+1)
+            lst[x-1]=lst[x]+1
 
-        if x*2 < 100000 and cnt[x*2]<3:
-            cnt[x*2]+=1
+    
+        if x!=0 and x*2<100001 and lst[x*2]==0:
             q.append(x*2)
-            lst[x*2]=min(lst[x*2],lst[x]+1)
-
-        # 목적지 3번 정도 봤으면 최솟값 나왔을거라 믿어
-        if cnt[y]>=3:
-            return
+            lst[x*2]=lst[x]+1
 
 n,k=map(int,sys.stdin.readline().split())
-lst=[100000]*100001
-
-# 한 2~3번 봤으면 제꺼야하나?
-cnt=[0]*100001
-
-lst[k]=abs(k-n)
-lst[n]=0
+lst=[0]*100001
 
 bfs(n,k)
 print(lst[k])
--- tmp.py
+++ tmp.py
@@ -1,1 +1,22 @@
-print(150**2, 66**2, 26**2)
+from collections import deque
+
+l = [0 for x in range(100002)]
+s,e = map(int,input().split())
+
+queue = deque()
+queue.append(s)
+
+while queue:
+    now = queue.popleft()
+    if now-1 > 0 and l[now-1] == 0:
+        l[now-1] = l[now]+1
+        queue.append(now-1)
+    if now+1 <= 100001 and l[now+1] == 0:
+        l[now+1] = l[now]+1
+        queue.append(now+1)
+    if now*2 <= 100001 and l[now*2] == 0:
+        l[now*2] = l[now]+1
+        queue.append(now*2)
+    
+l[s] = 0
+print(l[e])
