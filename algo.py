c8bd43c820c6952de7ffbcccc6ce3634abb5d19a
hobbang <hocheol0303@gmail.com>
2024-09-03 08:06:01 +0900

    7662_RUN!!!

--- algo.py
+++ algo.py
@@ -1,36 +1,48 @@
 '''
-힙에는 양수만 넣고 음수양수 정보를 딕셔너리로 관리
+첫 줄: TC
+시작마다: 입력 개수
+I n: n을 q에 삽입하는 연산
+D 1: Q에서 최댓값을 삭제하는 연산
+D -1: Q에서 최솟값을 삭제하는 연산
 '''
-
-import heapq
 import sys
-n=int(sys.stdin.readline())
-heap=[]
-info={}
+import heapq
 
-for _ in range(n):
-    input_ = int(sys.stdin.readline())
-    if input_ == 0:
-        if len(heap) == 0:
-            print(0)
-            continue
+t=int(sys.stdin.readline())
+
+for _ in range(t):
+    n=int(sys.stdin.readline())
+    min_heap=[]
+    max_heap=[]
+    cnt=0
+    for __ in range(n):
+        op, num = sys.stdin.readline().rstrip().split()
+        num=int(num)
+
+        if op == 'I':
+            heapq.heappush(min_heap, num)
+            heapq.heappush(max_heap, -num)
+            cnt+=1
         else:
-            output=heapq.heappop(heap)
-            if info[output]['-'] == 0:
-                info[output]['+']-=1
-                print(output)
+            if cnt==0:
+                min_heap=[]
+                max_heap=[]
+                continue
+            cnt-=1
+            if num == 1:
+                heapq.heappop(max_heap)
             else:
-                info[output]['-']-=1
-                print(-output)
-
+                heapq.heappop(min_heap)
+    if cnt==0:
+        print('EMPTY')
     else:
-        abs_ = abs(input_)
-        if abs_ not in info.keys():
-            info[abs_]={'+':0, '-':0}
-        
-        heapq.heappush(heap, abs_)
-
-        if input_ > 0:
-            info[abs_]['+']+=1
-        else:
-            info[abs_]['-']+=1
+        for i in max_heap:
+            max_=-heapq.heappop(max_heap)
+            if max_ in min_heap:
+                break
+        for i in min_heap:
+            min_=-heapq.heappop(min_heap)
+            if min_ in max_heap:
+                min_=-min_
+                break
+        print(max_, min_)
--- tmp.py
+++ tmp.py
@@ -1,23 +1,8 @@
-from collections import deque
+import sys
+import heapq
 
-l = [0 for x in range(100002)]
-s,e = map(int,input().split())
+lst=[]
+for i in [2,7,0,5,8]:
+    heapq.heappush(lst, -i)
+print(lst)
 
-queue = deque()
-queue.append(s)
-
-while queue:
-    now = queue.popleft()
-    if now-1 > 0 and l[now-1] == 0:
-        l[now-1] = l[now]+1
-        queue.append(now-1)
-    
-    if now+1 <= 100001 and l[now+1] == 0:
-        l[now+1] = l[now]+1
-        queue.append(now+1)
-    if now*2 <= 100001 and l[now*2] == 0:
-        l[now*2] = l[now]+1
-        queue.append(now*2)
-    
-l[s] = 0
-print(l[e])
