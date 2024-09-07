26986883a43f45aabba6e3868d49fee7c981dccd
hobbang <hocheol0303@gmail.com>
2024-09-07 11:21:12 +0900

    2579번 컨닝

--- algo.py
+++ algo.py
@@ -1,124 +1,28 @@
 '''
-ㅁ
-ㅣ
-ㄱ
-ㅗ
-ㄹ
+마지막 지점 꼭 밟 -> 마지막 지점에서 출발
+
+한 번에 1칸 or 2칸 ㄱㄴ
+연속 3칸 안됨
+ㅅㅂ
+
+컨닝
+dp == 밟아온 계단 발자취
+각 계단마다 최적의 이전 발자취 골라 >>>> 3칸 고려하기 꽁짜
 '''
-# row, col 주면 거기서 시계로 돌리고 대칭하고 시계로 또돌려
-
-def ㅁ(row, col):
-    global n, m, lst, max_
-    d_row = [0, 0, 1, 1]
-    d_col = [0, 1, 0, 1]
-
-    result=0
-    for i in range(4):
-        r = row+d_row[i]
-        c = col+d_col[i]
-        if r < 0 or c < 0 or r >= n or c >= m:
-            result=0
-            break
-        result+=lst[r][c]
-
-    max_ = max(max_, result)
-
-
-def ㅣ(row, col):
-    global n, m, lst, max_
-    d_rows = [[0,0,0,0], [0,1,2,3]]
-    d_cols = [[0,1,2,3], [0,0,0,0]]
-
-    for tc in range(len(d_rows)):
-        d_row = d_rows[tc]
-        d_col = d_cols[tc]
-        result=0
-        
-        for i in range(4):
-            r = row+d_row[i]
-            c = col+d_col[i]
-            if r < 0 or c < 0 or r >= n or c >= m:
-                result=0
-                break
-            result+=lst[r][c]
-
-        max_ = max(max_, result)
-
-
-def ㄱ(row, col):
-    global n, m, lst, max_
-    d_rows = [[0,0,0,1], [0,0,-1,-2], [0,1,1,1], [0,0,1,2],    [0,-1,-1,-1], [0,0,1,2], [0,0,0,-1], [0,1,2,2]]
-    d_cols = [[0,1,2,2], [0,1,1,1],   [0,0,1,2], [0,-1,-1,-1], [0,0,1,2],    [0,1,1,1], [0,1,2,2],  [0,0,0,1]]
-
-    for tc in range(len(d_rows)):
-        d_row = d_rows[tc]
-        d_col = d_cols[tc]
-        result=0
-        
-        for i in range(4):
-            r = row+d_row[i]
-            c = col+d_col[i]
-            if r < 0 or c < 0 or r >= n or c >= m:
-                result=0
-                break
-            result+=lst[r][c]
-            
-        max_ = max(max_, result)
-
-
-def ㅗ(row, col):
-    global n, m, lst, max_
-    d_rows = [[0,-1,0,0], [0,-1,0,1], [0,0,1,0],  [0,-1,1,0]]
-    d_cols = [[0,0,1,-1], [0,0,1,0],  [0,1,0,-1], [0,0,0,-1]]
-
-    for tc in range(len(d_rows)):
-        d_row = d_rows[tc]
-        d_col = d_cols[tc]
-        result=0
-        
-        for i in range(4):
-            r = row+d_row[i]
-            c = col+d_col[i]
-            if r < 0 or c < 0 or r >= n or c >= m:
-                result=0
-                break
-            result+=lst[r][c]
-            
-        max_ = max(max_, result)
-
-def ㄹ(row, col):
-    global n, m, lst, max_
-    d_rows = [[0,0,1,1], [0,1,1,2],   [0,0,1,1],    [0,1,1,2]]
-    d_cols = [[0,1,1,2], [0,0,-1,-1], [0,-1,-1,-2], [0,0,1,1]]
-
-    for tc in range(len(d_rows)):
-        d_row = d_rows[tc]
-        d_col = d_cols[tc]
-        result=0
-        
-        for i in range(4):
-            r = row+d_row[i]
-            c = col+d_col[i]
-            if r < 0 or c < 0 or r >= n or c >= m:
-                result=0
-                break
-            result+=lst[r][c]
-            
-        max_ = max(max_, result)
 
 import sys
-n, m = map(int,sys.stdin.readline().split())
-lst=[]
-max_=0
-for _ in range(n):
-    lst.append(list(map(int,sys.stdin.readline().split())))
 
-for row in range(n):
-    for col in range(m):
-        ㅁ(row, col)
-        ㅣ(row, col)
-        ㄱ(row, col)
-        ㅗ(row, col)
-        ㄹ(row, col)
+n=int(sys.stdin.readline())
+dp=[0]*(n+1)
+steps=[0]*(n+1)
 
-print(max_)
+for i in range(1,n+1):
+    steps[i]=int(sys.stdin.readline())
+
+dp[1] = steps[1]
+dp[2] = steps[1]+steps[2]
+
+for i in range(3, n+1):
+    dp[i]=max(dp[i-2]+steps[i], dp[i-3]+steps[i-1]+steps[i])    
+
+print(dp[-1])
