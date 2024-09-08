bf57bc7cd0ae5de2960b61d83392fe82af05244d
hobbang <hocheol0303@gmail.com>
2024-09-08 11:53:44 +0900

    7662

--- algo.py
+++ algo.py
@@ -4,6 +4,11 @@
 I n: n을 q에 삽입하는 연산
 D 1: Q에서 최댓값을 삭제하는 연산
 D -1: Q에서 최솟값을 삭제하는 연산
+
+동기화 문제 못 풀었음 -> 딕셔너리로 해결하기 컨닝
+
+틀: 출력 전에 max_ 꺼내고 nums[tmp] -= 1 추가
+2틀: elif에 출력 추가
 '''
 import sys
 import heapq
@@ -15,11 +20,17 @@ for _ in range(t):
     min_heap=[]
     max_heap=[]
     cnt=0
+
+    nums={}
     for __ in range(n):
         op, num = sys.stdin.readline().rstrip().split()
         num=int(num)
 
         if op == 'I':
+            if num in nums:
+                nums[num]+=1
+            else:
+                nums[num] = 1
             heapq.heappush(min_heap, num)
             heapq.heappush(max_heap, -num)
             cnt+=1
@@ -27,22 +38,50 @@ for _ in range(t):
             if cnt==0:
                 min_heap=[]
                 max_heap=[]
+                nums={}
                 continue
-            cnt-=1
-            if num == 1:
-                heapq.heappop(max_heap)
             else:
-                heapq.heappop(min_heap)
+                cnt-=1
+                if num == 1:
+                    tmp = -heapq.heappop(max_heap)
+                    while True:
+                        if nums[tmp] != 0:
+                            nums[tmp] -= 1
+                            break
+                        else:
+                            tmp = -heapq.heappop(max_heap)
+                else:
+                    tmp = heapq.heappop(min_heap)
+                    while True:
+                        if nums[tmp] != 0:
+                            nums[tmp] -= 1
+                            break
+                        else:
+                            tmp = heapq.heappop(min_heap)
+
+
+
+    max_=None
+    min_=None
     if cnt==0:
         print('EMPTY')
-    else:
-        for i in max_heap:
-            max_=-heapq.heappop(max_heap)
-            if max_ in min_heap:
+    elif cnt==1:
+        while min_heap:
+            tmp=heapq.heappop(min_heap)
+            if nums[tmp] != 0:
+                min_ = max_ = tmp
                 break
-        for i in min_heap:
-            min_=-heapq.heappop(min_heap)
-            if min_ in max_heap:
-                min_=-min_
+        print(max_, min_)
+    else:
+        while max_heap:
+            tmp=-heapq.heappop(max_heap)
+            if nums[tmp] != 0:
+                nums[tmp] -= 1
+                max_=tmp
+                break
+        while min_heap:
+            tmp=heapq.heappop(min_heap)
+            if nums[tmp] != 0:
+                min_=tmp
                 break
         print(max_, min_)
