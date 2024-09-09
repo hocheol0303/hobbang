a26840be9f0c44607a87cb3516a0050996115ad0
hobbang <hocheol0303@gmail.com>
2024-09-09 09:16:36 +0900

    20301_too_late

--- algo.py
+++ algo.py
@@ -1,87 +1,46 @@
 '''
-첫 줄: TC
-시작마다: 입력 개수
-I n: n을 q에 삽입하는 연산
-D 1: Q에서 최댓값을 삭제하는 연산
-D -1: Q에서 최솟값을 삭제하는 연산
+(N,K)-요세푸스 순열
+N명이 원형으로 앉아있음
+K번 사람 지우기 시작해서 그 사람의 오른쪽 K번째 사람 지움
+모든 사람 제거되었을 때 제거된 사람의 순서
 
-동기화 문제 못 풀었음 -> 딕셔너리로 해결하기 컨닝
+반전 요세푸스: M번 지울 때마다 반대 방향으로 돌림
 
-틀: 출력 전에 max_ 꺼내고 nums[tmp] -= 1 추가
-2틀: elif에 출력 추가
+(7,3): [1,2,3,4,5,6,7]: 3,6,2,7,5,1,4
 '''
 
 import sys
 
-t = int(sys.stdin.readline())
+n,k,m = map(int,sys.stdin.readline().split())
 
-for _ in range(t):
-    n=int(sys.stdin.readline())
-    min_heap=[]
-    max_heap=[]
-    cnt=0
+lst = [0 for i in range(n+1)]
+result=[]
+# True == 우, False == 좌
+direction=True
+loc=0
+cnt=0
 
-    nums={}
-    for __ in range(n):
-        op, num = sys.stdin.readline().rstrip().split()
-        num=int(num)
+# # 1씩 올리는거 너무 느릴 것 같음
+# while True:
+#     # k칸 옮김
+#     for i in range(k):
+#         # 한 칸씩 옮겨감 index 넘어갔을 때 처리
+#         while True:
+#             if direction:
+#                 loc += 1
+#                 if loc > n:
+#                     loc = 1
+#             else:
+#                 loc -= 1
+#                 if loc < 1:
+#                     loc = n
+#             if lst[loc] == 0:
+#                 break
+#     lst[loc] = 1
+#     result.append(loc)
+#     cnt+=1
+#     if cnt == m:
+#         direction = False
+#         cnt=0
+#     if len(result) == n : break
 
-        if op == 'I':
-            if num in nums:
-                nums[num]+=1
-            else:
-                nums[num] = 1
-            heapq.heappush(min_heap, num)
-            heapq.heappush(max_heap, -num)
-            cnt+=1
-        else:
-            if cnt==0:
-                min_heap=[]
-                max_heap=[]
-                nums={}
-                continue
-            else:
-                cnt-=1
-                if num == 1:
-                    tmp = -heapq.heappop(max_heap)
-                    while True:
-                        if nums[tmp] != 0:
-                            nums[tmp] -= 1
-                            break
-                        else:
-                            tmp = -heapq.heappop(max_heap)
-                else:
-                    tmp = heapq.heappop(min_heap)
-                    while True:
-                        if nums[tmp] != 0:
-                            nums[tmp] -= 1
-                            break
-                        else:
-                            tmp = heapq.heappop(min_heap)
-
-
-
-    max_=None
-    min_=None
-    if cnt==0:
-        print('EMPTY')
-    elif cnt==1:
-        while min_heap:
-            tmp=heapq.heappop(min_heap)
-            if nums[tmp] != 0:
-                min_ = max_ = tmp
-                break
-        print(max_, min_)
-    else:
-        while max_heap:
-            tmp=-heapq.heappop(max_heap)
-            if nums[tmp] != 0:
-                nums[tmp] -= 1
-                max_=tmp
-                break
-        while min_heap:
-            tmp=heapq.heappop(min_heap)
-            if nums[tmp] != 0:
-                min_=tmp
-                break
-        print(max_, min_)
