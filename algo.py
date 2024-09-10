0c587e3715b2a2896ee37ec479660c9a75ec2998
hobbang <hocheol0303@gmail.com>
2024-09-10 09:35:31 +0900

    2458_annotation

--- algo.py
+++ algo.py
@@ -15,6 +15,8 @@ for i in range(m):
     grp[start][end]=1
 
 
+
+# 출발 - 도착 최단거리 저장 행은 출발지정보
 # 경유지반복(출발지반복(도착지반복)): 정해진 규칙임
 for i in range(1, n+1):         # 경유지
     for j in range(1, n+1):     # 출발지
@@ -25,10 +27,11 @@ for i in range(1, n+1):         # 경유지
 
 result = 0
 
+# n행의 숫자 개수 + n열의 숫자 개수 == 관련된 노드 개수
 for i in range(1, n+1):
     cnt=0
     for j in range(1, n+1):
-        if grp[i][j] != float('inf') or grp[j][i] != float('inf'):
+        if grp[i][j] != float('inf') or grp[j][i] != float('inf'):   # 오고가고를 모두 체크
             cnt+=1
 
     if cnt == n-1:
