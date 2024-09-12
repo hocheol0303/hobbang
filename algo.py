48017d224ae608f5ee5747731053acf1d1413f63
hobbang <hocheol0303@gmail.com>
2024-09-12 22:06:36 +0900

    annotation

--- algo.py
+++ algo.py
@@ -1,8 +1,6 @@
 '''
 n = 과일개수
 lst = 꽂음
-
-코드 해석하고 제출하세ㅛㅇ: https://blog.everdu.com/419
 '''
 
 import sys
@@ -27,6 +25,7 @@ while rear<n:
             # 지금까지의 최대 길이를 저장해놔
             result = max(result, rear-front)
 
+            # next_front가 아닌거 (먼저 들어온거) 골라내ㅏㄹ
             for i in range(len(species)):
                 if species[i] != lst[next_front]:
                     species.pop(i)
@@ -35,11 +34,9 @@ while rear<n:
             species.append(lst[rear])
             front = next_front
 
-    # 다르면 다음 단계에서 위 else문 실행할거여
     # next_front는 새로운 종의 첫 index
     if lst[rear-1] != lst[rear]:
         next_front=rear
-
     rear+=1
 
 result=max(result, n-front)
--- tmp.py
+++ tmp.py
@@ -1,6 +1,19 @@
+'''
+들고 있다가 3번째 나오면 정답후보 업데이트
+'''
 import sys
-lst=[]
-for i in range(3):
-    tmp=int(sys.stdin.read())
-    lst.append(tmp)
-print(lst)
+
+n = int(sys.stdin.readline())
+lst = list(map(int, sys.stdin.readline().split()))
+
+front = 0
+other=0
+rear = 1
+species=[]
+cnt=0
+
+while rear < n:
+    if lst[rear-1] != lst[rear]:
+        if lst[rear] not in species:
+            if len(species) < 2:
+                species.append(lst[rear])
