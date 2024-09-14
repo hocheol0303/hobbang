c90d0bc5434816e1bef1ff7f7e6ff12ee638c6f7
hobbang <hocheol0303@gmail.com>
2024-09-14 18:32:17 +0900

    2477_계산 잘못함)

--- algo.py
+++ algo.py
@@ -1,22 +1,26 @@
 '''
-두 수를 곱한 뒤 최대공약수로 나누면 최소공배수
-유클리드 호제법: 최대 공약수 구하는 방법
-    두 수 A > B
-    A % B = C -> B % C = D -> C % D = E -> ...(나머지가 0이 될 때까지) -> X % Y = Z = 0일 때, A * B // Y = 최소공배수
-
+계단을 찾기 리스트 빙빙 돌리면 계단으로 시작하는 모양으로 고정됨 -> 계단으로 고정된 모양의 전, 전전이 온전한 직사각형의 변 2개
 '''
 
 import sys
-a, b = map(int,sys.stdin.readline().split())
-target=a*b
 
-if a == 1 or b == 1:
-    print(a*b)
-else:
-    a,b = max(a,b), min(a,b)
+num_corn=int(sys.stdin.readline())
+num_count=[0]*7
+lst=[]
 
-    while a % b != 0:
-        # print(a,b)
-        a, b = b, a%b
+for i in range(6):
+    lst.append(tuple(map(int, sys.stdin.readline().split())))
+    num_count[lst[-1][0]]+=1
 
-    print(target // b)
+while True:
+    if lst[0][0] == lst[2][0] and lst[1][0] == lst[3][0]:
+        break
+    else:
+        lst.append(lst.pop(0))
+
+
+
+# print(lst)
+result = (lst[-1][1] * lst[-2][1]) - (lst[1][1] * lst[2][1] * num_corn)
+# print(lst[-1], lst[-2], lst[1], lst[2], num_corn)
+print(result)
