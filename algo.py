dbd628741c1fa5a1eb32ed25f88f0e9e9c91cc14
hobbang <hocheol0303@gmail.com>
2024-09-19 09:14:41 +0900

    6064

--- algo.py
+++ algo.py
@@ -1,62 +1,40 @@
 '''
-게임 크기: 10x10
-주사위 q : 1~6
-i번에 있고 q가 나오면 i+q로 이동
-주사위 결과가 100번 넘어가면 이동 불가
-사다리는 숫자 커지게 이동
-뱀은 숫자 작아지게 이동
+m:n == 마지막 해
 
-사다리 수 n, 뱀의 수 m
-n개 줄에 사다리 정보 (x번에 도착하면 y번으로 이동)
-m개 줄에 뱀 정보 (u -> v)
-1번, 100번은 뱀 x
+첫 해 = 1:1
+둘째 해 = 2:2
 
-1에서 100을 향한 최소 이동 횟수 출력
+x' = x+1 if x < m, else x' = 1
+y' = y+1 if y < n, else y' = 1
 '''
 
 '''
-틀림: 뱀 / 사다리 만나면 거기에 머무를 수 있게 짰는데 아닌가봄 무조건 이동되는거 90 -> 1인 뱀 있을 때 90 밟으면 무조건 1인거
+1씩 더하는 코드는 시간초과
+적절한 점화식이 필요해.?
+
+컨닝합니다
+m=10, n=12일 때
+x=1, y=11 : count = 11 = m+1
+x=3, y=1 : count = 13 = n+1
+    => (count-x)%m==0 and (count-y)%n==0일 때 정답 -> 1부터 1씩 올리는거 시간초과
+    얼마씩 올리야 정답될까 -> count=x부터 m씩 키우며 -> 어케나옴 : x,y를 모두 변수(=2변수)로 푸는 것보다 하나 고정하고 푸는게 낫다(=1변수). : count=x로 놓고 m씩 더하기 or count=y로 놓고 n씩 더하기 택1
 '''
 
 import sys
-from collections import deque
 
-def bfs():
-    global lst, times
-    q = deque()
-    q.append(1)
-    times[1] = 0
-    d_x = [1,2,3,4,5,6]
+tc = int(sys.stdin.readline())
 
-    while q:
-        x = q.popleft()
-
-        for i in range(6):
-            n_x = x+d_x[i]
-
-            if n_x > 100:
-                continue
-            if lst[n_x] == n_x and times[n_x] > times[x]+1:
-                times[n_x] = times[x]+1
-                q.append(n_x)
-            elif lst[n_x] != n_x:
-                if times[lst[n_x]] > times[x]+1:
-                    times[lst[n_x]] = times[x]+1
-                    q.append(lst[n_x])
-
-
-n, m = map(int, sys.stdin.readline().split())
-
-lst = [i for i in range(101)]
-times = [float('inf')] * 101
-
-for _ in range(n):
-    start, end = map(int, sys.stdin.readline().split())
-    lst[start] = end
-
-for _ in range(m):
-    start, end = map(int, sys.stdin.readline().split())
-    lst[start] = end
-
-bfs()
-print(times[100])
+for _ in range(tc):
+    m, n, x, y = map(int, sys.stdin.readline().split())
+    cnt=y
+    check=False
+    while cnt <= m*n:
+        if (cnt-x)%m == 0 and (cnt-y)%n == 0:
+            check=True
+            break
+        else:
+            cnt+=n
+    if check:
+        print(cnt)
+    else:
+        print(-1)
