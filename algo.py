aec529520173ed8946460585e8f3dee3842efc61
hobbang <hocheol0303@gmail.com>
2024-09-21 17:16:17 +0900

    18111

--- algo.py
+++ algo.py
@@ -1,40 +1,62 @@
 '''
-m:n == 마지막 해
+블록 제거 2초
+블록 설치 1초
+최대 높이 256
 
-첫 해 = 1:1
-둘째 해 = 2:2
-
-x' = x+1 if x < m, else x' = 1
-y' = y+1 if y < n, else y' = 1
+최소시간 필요
+2개 이상 -> 높은거
+땅 고르게 만드는데 걸리는 시간, 높이 출력
 '''
 
 '''
-1씩 더하는 코드는 시간초과
-적절한 점화식이 필요해.?
+모든 칸의 높이가 같아야함
+lst에는 index에 해당하는 높이를 가진 칸의 수를 저장
+따라서 sum(lst) = m*n
 
-컨닝합니다
-m=10, n=12일 때
-x=1, y=11 : count = 11 = m+1
-x=3, y=1 : count = 13 = n+1
-    => (count-x)%m==0 and (count-y)%n==0일 때 정답 -> 1부터 1씩 올리는거 시간초과
-    얼마씩 올리야 정답될까 -> count=x부터 m씩 키우며 -> 어케나옴 : x,y를 모두 변수(=2변수)로 푸는 것보다 하나 고정하고 푸는게 낫다(=1변수). : count=x로 놓고 m씩 더하기 or count=y로 놓고 n씩 더하기 택1
+거리로 생각하나?: 한 칸 위로는 1의 시간 들고 b-=1, 한 칸 아래로는 2의 시간 들고 b+=1
+어느 높이가 최소 시간일지 탐색 : 높이를 결정할 변수 놓기
 '''
 
 import sys
 
-tc = int(sys.stdin.readline())
+n, m, b = map(int, sys.stdin.readline().split())
+lst=[0] * 257
+min_ = 256
+max_ = 0
+results=[]
 
-for _ in range(tc):
-    m, n, x, y = map(int, sys.stdin.readline().split())
-    cnt=y
-    check=False
-    while cnt <= m*n:
-        if (cnt-x)%m == 0 and (cnt-y)%n == 0:
-            check=True
-            break
+for _ in range(n):
+    blocks = list(map(int, sys.stdin.readline().split()))
+    for item in blocks:
+        lst[item] += 1
+        # 깔려 있는 블록의 최대 높이, 최소 높이 가지고있어
+        if item > max_:
+            max_ = item
+        if item < min_:
+            min_ = item
+
+for target_height in range(min_, max_+1):
+    # 아래 반복 끝났는데 num_of_required가 양수면 블록 부족한거
+    num_of_required=-b
+    time=0
+    for now_height in range(min_, max_+1):
+        if target_height == now_height:
+            continue
+        elif target_height > now_height:
+            # 쌓는 차례
+            num_of_required += (target_height - now_height) * lst[now_height]
+            time += (target_height-now_height) * lst[now_height]
         else:
-            cnt+=n
-    if check:
-        print(cnt)
+            # 깎을 차례
+            num_of_required -= (now_height - target_height) * lst[now_height]
+            time += (now_height - target_height) * lst[now_height] * 2
+    if num_of_required > 0:
+        continue
     else:
-        print(-1)
+        results.append((time, target_height))
+            
+
+# print(lst)
+# print(min_, max_)
+results.sort(key = lambda x : (x[0], -x[1]))
+print(results[0][0], results[0][1])
--- tmp.py
+++ tmp.py
@@ -1,3 +1,3 @@
-import heapq
-
-print(heapq.heappop([1,2]))
+lst = [(2,1), (0,5), (4,4), (5,3), (0,3), (2,5)]
+lst.sort(key=lambda x:(x[0], -x[1]))
+print(lst)
