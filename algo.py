4e9587d65666620b0fa3f9735eaa7e9e269dfb5d
hobbang <hocheol0303@gmail.com>
2024-09-23 07:43:23 +0900

    16953

--- algo.py
+++ algo.py
@@ -1,28 +1,45 @@
 import sys
+from collections import deque
 
-input_str = sys.stdin.readline().rstrip()
-result=''
-stack=[]
+'''
+맨 뒤에 1 추가하거나 2를 곱하거나
+'''
 
-for i in input_str:
-    if i.isalpha():
-        result+=i
-    elif i == '(':
-        stack.append(i)
-    elif i == ')':
-        while stack[-1] != '(':
-            result+=stack.pop()
-        stack.pop()
-    elif i == '*' or i == '/':
-        while stack and (stack[-1] == '*' or stack[-1] == '/'):
-            result+=stack.pop()
-        stack.append(i)
-    elif i == '+' or i == '-':
-        while stack and stack[-1] != '(':
-            result += stack.pop()
-        stack.append(i)
+'''
+탈출조건 때문에 계속 틀리는거 같은데,,,,,,,,,,,,,,,,,,,,,,
+'''
 
-while stack:
-    result += stack.pop()
 
-print(result)
+def bfs(start, end):
+    grp={}
+    q = deque()
+
+    time=1
+    grp[start]=time
+    q.append([start, time])
+
+    while q and end not in grp.keys():
+        start, time = q.popleft()
+        
+        if start > end:
+            continue
+
+        # 2 곱한거
+        next_node1 = start*2
+
+        # 1 붙인거
+        next_node2 = start*10+1
+
+        q.append([next_node1, time+1])
+        q.append([next_node2, time+1])
+        grp[next_node1] = time+1
+        grp[next_node2] = time+1
+    
+    if end in grp.keys():
+        print(grp[end])
+    else:
+        print(-1)
+
+
+a,b=map(int, sys.stdin.readline().split())
+bfs(a,b)
