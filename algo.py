8d60a96ac574436b5c01a57f5614a994ede79bfe
hobbang <hocheol0303@gmail.com>
2024-09-24 08:00:35 +0900

    1149

--- algo.py
+++ algo.py
@@ -1,45 +1,20 @@
 import sys
-from collections import deque
+n = int(sys.stdin.readline())
+lst=[]
+dp=[[float('inf')]*3 for _ in range(n)]
+for _ in range(n):
+    lst.append(list(map(int, sys.stdin.readline().split())))
 
-'''
-맨 뒤에 1 추가하거나 2를 곱하거나
-'''
+# print(lst)
+# dp[0][0]=1
+# print(dp)
 
-'''
-탈출조건 때문에 계속 틀리는거 같은데,,,,,,,,,,,,,,,,,,,,,,
-'''
+dp[0] = lst[0]
 
+for i in range(1,n):
+    lst[i], dp[i]
+    dp[i][0] = lst[i][0] + min(dp[i-1][1], dp[i-1][2])
+    dp[i][1] = lst[i][1] + min(dp[i-1][0], dp[i-1][2])
+    dp[i][2] = lst[i][2] + min(dp[i-1][0], dp[i-1][1])
 
-def bfs(start, end):
-    grp={}
-    q = deque()
-
-    time=1
-    grp[start]=time
-    q.append([start, time])
-
-    while q and end not in grp.keys():
-        start, time = q.popleft()
-        
-        if start > end:
-            continue
-
-        # 2 곱한거
-        next_node1 = start*2
-
-        # 1 붙인거
-        next_node2 = start*10+1
-
-        q.append([next_node1, time+1])
-        q.append([next_node2, time+1])
-        grp[next_node1] = time+1
-        grp[next_node2] = time+1
-    
-    if end in grp.keys():
-        print(grp[end])
-    else:
-        print(-1)
-
-
-a,b=map(int, sys.stdin.readline().split())
-bfs(a,b)
+print(min(dp[-1]))
