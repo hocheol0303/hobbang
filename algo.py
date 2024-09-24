e0b703a4d2cd6d8ab6ed30bb29bc8234a8413481
hobbang <hocheol0303@gmail.com>
2024-09-25 07:53:35 +0900

    13549

--- algo.py
+++ algo.py
@@ -1,20 +1,37 @@
+'''
+while q말고 end에 방문 시 break 하면 틀림
+시간 0 증가가 있어서 근가봄 
+dfs인가??
+'''
+
 import sys
-n = int(sys.stdin.readline())
-lst=[]
-dp=[[float('inf')]*3 for _ in range(n)]
-for _ in range(n):
-    lst.append(list(map(int, sys.stdin.readline().split())))
+from collections import deque
 
-# print(lst)
-# dp[0][0]=1
-# print(dp)
+def bfs():
+    global start, end
+    q = deque()
 
-dp[0] = lst[0]
+    lst=[float('inf')]*100001
+    q.append(start)
+    lst[start]=0
 
-for i in range(1,n):
-    lst[i], dp[i]
-    dp[i][0] = lst[i][0] + min(dp[i-1][1], dp[i-1][2])
-    dp[i][1] = lst[i][1] + min(dp[i-1][0], dp[i-1][2])
-    dp[i][2] = lst[i][2] + min(dp[i-1][0], dp[i-1][1])
+    while q:
+        start = q.popleft()
 
-print(min(dp[-1]))
+        if start*2 <= 100000:
+            if lst[start] < lst[start*2]:
+                lst[start*2] = lst[start]
+                q.append(start*2)
+        if start+1 <= 100000:
+            if lst[start]+1 < lst[start+1]:
+                lst[start+1] = lst[start]+1
+                q.append(start+1)
+        if start-1 >= 0:
+            if lst[start]+1 < lst[start-1]:
+                lst[start-1] =lst[start]+1
+                q.append(start-1)
+    print(lst[end])
+
+start, end = map(int, sys.stdin.readline().split())
+
+bfs()
