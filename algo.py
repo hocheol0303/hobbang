891fbe0c431009c06ed0f73c62b2df6d6b17681c
hobbang <hocheol0303@gmail.com>
2024-09-26 08:16:32 +0900

    2096_out of memory

--- algo.py
+++ algo.py
@@ -1,37 +1,28 @@
-'''
-while q말고 end에 방문 시 break 하면 틀림
-시간 0 증가가 있어서 근가봄 
-dfs인가??
-'''
-
 import sys
-from collections import deque
+n=int(sys.stdin.readline())
 
-def bfs():
-    global start, end
-    q = deque()
+lst=[]
+max_dp=[[0]*3 for _ in range(n)]
+min_dp=[[0]*3 for _ in range(n)]
 
-    lst=[float('inf')]*100001
-    q.append(start)
-    lst[start]=0
+for _ in range(n):
+    a, b, c = map(int, sys.stdin.readline().split())
+    lst.append([a, b, c])
 
-    while q:
-        start = q.popleft()
+for i in range(3):
+    max_dp[0][i] = min_dp[0][i] = lst[0][i]
 
-        if start*2 <= 100000:
-            if lst[start] < lst[start*2]:
-                lst[start*2] = lst[start]
-                q.append(start*2)
-        if start+1 <= 100000:
-            if lst[start]+1 < lst[start+1]:
-                lst[start+1] = lst[start]+1
-                q.append(start+1)
-        if start-1 >= 0:
-            if lst[start]+1 < lst[start-1]:
-                lst[start-1] =lst[start]+1
-                q.append(start-1)
-    print(lst[end])
+for i in range(1,n):
+    max_dp[i][0] = lst[i][0] + max(max_dp[i-1][0], max_dp[i-1][1])
+    max_dp[i][1] = lst[i][1] + max(max_dp[i-1][0], max_dp[i-1][1], max_dp[i-1][2])
+    max_dp[i][2] = lst[i][2] + max(max_dp[i-1][1], max_dp[i-1][2])
 
-start, end = map(int, sys.stdin.readline().split())
+    min_dp[i][0] = lst[i][0] + min(min_dp[i-1][0], min_dp[i-1][1])
+    min_dp[i][1] = lst[i][1] + min(min_dp[i-1][0], min_dp[i-1][1], min_dp[i-1][2])
+    min_dp[i][2] = lst[i][2] + min(min_dp[i-1][1], min_dp[i-1][2])
 
-bfs()
+# print(lst)
+# print(min_dp)
+# print(max_dp)
+
+print(max(max_dp[-1]), min(min_dp[-1]))
