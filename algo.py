5872b4fffb5ab1f16231262288cbba8125c1c9d8
hobbang <hocheol0303@gmail.com>
2024-09-28 15:17:00 +0900

    12865

--- algo.py
+++ algo.py
@@ -1,52 +1,27 @@
+'''
+컨닝: 2차원 DP
+dp[i][j] = 최대 무게 i, j번째 아이템까지 살펴봤을 때의 최대 가치
+
+1차원으로 짜면 새로운 아이템이 들어왔고 바로 담지 않았을 경우 이후에 들어온 아이템과의 조합이 현재 담은 아이템 조합보다 가치가 높을 경우 알아낼 수 없다.
+들어올 아이템 n, 최대 무게 k일 때 n * k의 2차원 배열을 사용
+
+담을 아이템의 인덱스를 i, 가방의 허용 용량(가정)이 j, 담을 물건의 무게를 weight, 가치를 value로 둘 때
+    1. 들어온 아이템의 무게가 가정한 허용 용량 j보다 클 때: j < weight: dp[i][j] = dp[i-1][j]
+    2. 1이 아닐 때: j >= weight: dp[i][j] = max(dp[i-1][j-weight]+value, dp[i-1][j])
+'''
+
 import sys
-n=int(sys.stdin.readline())
 
-# 이전 단계의 값들 저장하는거
-dp_max=[0,0,0]
-dp_min=[0,0,0]
-min_ = max_ = None
+n, k = map(int, sys.stdin.readline().split())
+dp = [[0]*(k+1)]
 
-for i in range(n):
-    a, b, c = map(int, sys.stdin.readline().split())
-    if i == 0:
-        dp_max=[a,b,c]
-        dp_min=[a,b,c]
-    else:
-        new_max_a = a + max(dp_max[0], dp_max[1])
-        new_max_b = b + max(dp_max[0], dp_max[1], dp_max[2])
-        new_max_c = c + max(dp_max[1], dp_max[2])
-        dp_max = [new_max_a, new_max_b, new_max_c]
+for i in range(1, n+1):
+    dp.append([0]*(k+1))
+    weight, value = map(int, sys.stdin.readline().split())
+    for j in range(1, k+1):
+        if j < weight:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i-1][j-weight]+value, dp[i-1][j]) # 안넣었을 때의 무게와 들어온 무게 뺀 무게에서 새 아이템 넣었을 때의 무게 비교
 
-        new_min_a = a + min(dp_min[0], dp_min[1])
-        new_min_b = b + min(dp_min[0], dp_min[1], dp_min[2])
-        new_min_c = c + min(dp_min[1], dp_min[2])
-        dp_min = [new_min_a, new_min_b, new_min_c]
-
-print(max(dp_max), min(dp_min))
-
-########################################################################
-# for _ in range(n):
-#     a, b, c = map(int, sys.stdin.readline().split())
-#     lst.append([a, b, c])
-
-# for i in range(3):
-#     dp[0][i] = lst[0][i]
-# for i in range(1,n):
-#     dp[i][0] = lst[i][0] + max(dp[i-1][0], dp[i-1][1])
-#     dp[i][1] = lst[i][1] + max(dp[i-1][0], dp[i-1][1], dp[i-1][2])
-#     dp[i][2] = lst[i][2] + max(dp[i-1][1], dp[i-1][2])
-
-# print(max(dp[-1]),end=' ')
-
-# for i in range(3):
-#     dp[0][i] = lst[0][i]
-# for i in range(1,n):
-#     dp[i][0] = lst[i][0] + min(dp[i-1][0], dp[i-1][1])
-#     dp[i][1] = lst[i][1] + min(dp[i-1][0], dp[i-1][1], dp[i-1][2])
-#     dp[i][2] = lst[i][2] + min(dp[i-1][1], dp[i-1][2])
-
-# print(min(dp[-1]))
-
-# print(lst)
-# print(min_dp)
-# print(max_dp)
+print(dp[-1][-1])
