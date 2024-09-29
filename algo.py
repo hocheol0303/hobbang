a2c0fa18b87049949052e80ee2fa4f8d3b3d199e
hobbang <hocheol0303@gmail.com>
2024-09-29 10:52:09 +0900

    1303

--- algo.py
+++ algo.py
@@ -1,27 +1,46 @@
-'''
-컨닝: 2차원 DP
-dp[i][j] = 최대 무게 i, j번째 아이템까지 살펴봤을 때의 최대 가치
-
-1차원으로 짜면 새로운 아이템이 들어왔고 바로 담지 않았을 경우 이후에 들어온 아이템과의 조합이 현재 담은 아이템 조합보다 가치가 높을 경우 알아낼 수 없다.
-들어올 아이템 n, 최대 무게 k일 때 n * k의 2차원 배열을 사용
-
-담을 아이템의 인덱스를 i, 가방의 허용 용량(가정)이 j, 담을 물건의 무게를 weight, 가치를 value로 둘 때
-    1. 들어온 아이템의 무게가 가정한 허용 용량 j보다 클 때: j < weight: dp[i][j] = dp[i-1][j]
-    2. 1이 아닐 때: j >= weight: dp[i][j] = max(dp[i-1][j-weight]+value, dp[i-1][j])
-'''
-
 import sys
+from collections import deque
 
-n, k = map(int, sys.stdin.readline().split())
-dp = [[0]*(k+1)]
+def bfs(row, col):
+    global n, m, lst, w, b
+    color = lst[row][col]
+    d_rows = [-1, 1, 0, 0]
+    d_cols = [0, 0, -1, 1]
+    q = deque()
+    q.append((row, col))
+    lst[row][col] = '.'
+    mass = 1
 
-for i in range(1, n+1):
-    dp.append([0]*(k+1))
-    weight, value = map(int, sys.stdin.readline().split())
-    for j in range(1, k+1):
-        if j < weight:
-            dp[i][j] = dp[i-1][j]
-        else:
-            dp[i][j] = max(dp[i-1][j-weight]+value, dp[i-1][j]) # 안넣었을 때의 무게와 들어온 무게 뺀 무게에서 새 아이템 넣었을 때의 무게 비교
+    while q:
+        row, col = q.popleft()
 
-print(dp[-1][-1])
+        for i in range(4):
+            n_row = row + d_rows[i]
+            n_col = col + d_cols[i]
+
+            if n_row >= m or n_col >= n or n_row < 0 or n_col < 0:
+                continue
+            elif lst[n_row][n_col] != color:
+                continue
+            else:
+                q.append((n_row, n_col))
+                lst[n_row][n_col] = '.'
+                mass += 1
+    
+    dct[color] += mass**2
+
+
+n, m = map(int, sys.stdin.readline().split())
+lst = []
+w = b = 0
+dct = {'W':0, 'B':0}
+for _ in range(m):
+    lst.append(list(map(str, sys.stdin.readline().rstrip())))
+
+for r in range(m):
+    for c in range(n):
+        if lst[r][c] == '.':
+            continue
+        bfs(r, c)
+
+print(dct['W'], dct['B'])
