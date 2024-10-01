4097aa377d880fe8d7ad67175a29c4b109969cd3
hobbang <hocheol0303@gmail.com>
2024-10-02 08:41:33 +0900

    15652

--- algo.py
+++ algo.py
@@ -1,26 +1,14 @@
-'''
-백트래킹 첫 문제
-
-1부터 n까지 중복 없는 m개의 수열
-
-모르겠저
-백츄래킹은 DFS 기반 재귀함수
-'''
-
 import sys
 
 def dfs(start):
     global lst, m
-    # 길이 맞으면 출력하고 나가
     if len(lst) == m:
         print(' '.join(map(str, lst)))
-        return
     else:
         for i in range(start, n+1):
-            if i not in lst:
-                lst.append(i)
-                dfs(i+1)
-                lst.pop()
+            lst.append(i)
+            dfs(i)
+            lst.pop()
 
 n, m = map(int, sys.stdin.readline().split())
 lst = []
