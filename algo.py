30b248f8ee14e38ff68d0dee0982eb0ae0f2e30a
hobbang <hocheol0303@gmail.com>
2024-10-05 15:27:35 +0900

    11725_hard_to_me

--- algo.py
+++ algo.py
@@ -1,17 +1,40 @@
 '''
-백트래킹인가 했는데 DP래
+순서 없는거 처리하는게 포인트
+두 숫자 모두 등장한 적 없을 수 있음
+    만들어져 있는 두 트리를 연결하는 일이 존재할 수 있따. == 누군가 연결되기 전까진 서로 누가 부모일지 모른다.
+
+고집이 너무 쎄 틀린건 틀린거여
 '''
 
 import sys
+from collections import deque
+
+def bfs():
+    global has_parent, dct
+    q = deque()
+    q.append(1)
+
+    while q:
+        start = q.popleft()
+
+        for i in dct[start]:
+            if has_parent[i] == -1:
+                has_parent[i] = start
+                q.append(i)
 
 n = int(sys.stdin.readline())
+dct = {i:[] for i in range(1, n+1)}
+has_parent = [-1]*(n+1)
 
-lst = tuple(map(int, sys.stdin.readline().split()))
-dp = [1]*(n)
+has_parent[1] = 1
 
-for i in range(1, n):
-    for j in range(i):
-        if lst[i] > lst[j]:
-            dp[i] = max(dp[i], dp[j]+1)
+for _ in range(n-1):
+    a, b = map(int, sys.stdin.readline().split())
+    dct[a].append(b)
+    dct[b].append(a)
 
-print(max(dp))
+bfs()
+
+# print(dct)
+for i in has_parent[2:]:
+    print(i)
