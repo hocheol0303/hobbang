0d19d0a6423f10628d5790e8562f8f3ceb0a72a1
hobbang <hocheol0303@gmail.com>
2024-10-07 09:56:44 +0900

    15666

--- algo.py
+++ algo.py
@@ -1,18 +1,23 @@
 import sys
 
 def dfs(start):
-    global n, m, lst
+    global n, m, lst, did
     if len(result) == m:
-        print(*result)
-        result.pop()
+        if tuple(result) in did:
+            pass
+        else:
+            print(*result)
+            did.add(tuple(result))
     else:
         for i in range(start, n):
             result.append(lst[i])
             dfs(i)
+            result.pop()
 
 
 n, m = map(int, sys.stdin.readline().split())
 lst = sorted(list(map(int, sys.stdin.readline().split())))
 result=[]
+did=set()
 
 dfs(0)
