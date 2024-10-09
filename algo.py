d841b1c17a770f5096ff377a35aab7c211924824
hobbang <hocheol0303@gmail.com>
2024-10-10 08:40:44 +0900

    11660

--- algo.py
+++ algo.py
@@ -1,50 +1,26 @@
+'''
+변수 범위 보라 시간초과가 날 수밖에 없다 (1초 - 1024*1024*100000)
+DP(점화식)로 풀어야 한다 -> 어케했노;;
+먼저 dp 테이블을 (0,0)에서 현위치까지의 합으로 채워
+행렬 그림 그리면서 dp 테이블을 어떻게 활용할지 생각
+컨닝 좋은 블로그: https://jominseoo.tistory.com/101
+'''
+
 import sys
 
-'''
-dp 지나가면서 행별로 max값 두 개씩 정해야함
-'''
-
-t = int(sys.stdin.readline())
-
-for _ in range(t):
-    max_=0
-    n = int(sys.stdin.readline())
-    lst=[]
-    dp=[[0]*n for i in range(2)]
+n, m = map(int, sys.stdin.readline().split())
+lst=[]
+dp = [[0]*(n+1) for i in range(n+1)]
+for i in range(n):
     lst.append(list(map(int, sys.stdin.readline().split())))
-    lst.append(list(map(int, sys.stdin.readline().split())))
-    
-    if n == 1:
-        print(max(lst[0][0], lst[1][0]))
-    elif n == 2:
-        print(max(lst[0][1]+lst[1][0], lst[0][0]+lst[1][1]))
-    else:   
-        dp[0][0] = lst[0][0]
-        dp[1][0] = lst[1][0]
 
-        dp[0][1] = lst[1][0] + lst[0][1]
-        dp[1][1] = lst[0][0] + lst[1][1]
-        
-        # 각 행의 max 2개씩 : 0행 계산용, 1행 계산용
-        max_list = [[dp[0][0],dp[0][1]],
-                    [dp[1][0],dp[1][1]]]
-        # 0행 계산 시: 0번과 3번 비교
-        # 1행 계산 시: 1번과 2번 비교
-        
-        for i in range(2, n):
-            dp[0][i] = lst[0][i] + max(max_list[0][0], max_list[1][0], max_list[1][1])
-            dp[1][i] = lst[1][i] + max(max_list[0][0], max_list[0][1], max_list[1][0])
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        dp[i][j] = dp[i-1][j] + dp[i][j-1] + lst[i-1][j-1] - dp[i-1][j-1]
 
-            max_list = [[max(max_list[0][0], max_list[0][1]), dp[0][i]], 
-                        [max(max_list[1][0], max_list[1][1]), dp[1][i]]]
+for _ in range(m):
+    result=0
+    r1, c1, r2, c2 = map(int, sys.stdin.readline().split())
 
-            max_ = max(dp[0][i], dp[1][i], max_)
-        
-        print(max_)
-    # print()
-    # for l in lst:
-    #     print(l)
-    # print()
-    # for d in dp:
-    #     print(d)
-    # print()
+    result = dp[r2][c2] - dp[r2][c1-1] - dp[r1-1][c2] + dp[r1-1][c1-1]
+    print(result)
