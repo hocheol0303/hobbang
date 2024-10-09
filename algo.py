07247d538a6c745163620afc8464a2fc5c7c5336
hobbang <hocheol0303@gmail.com>
2024-10-09 12:05:40 +0900

    9465

--- algo.py
+++ algo.py
@@ -1,50 +1,50 @@
-'''
-DP
-배열 1부터 시작
-n층: 왼쪽은 +n-1, 오른쪽은 +n
-'''
-
 import sys
 
-floor = int(sys.stdin.readline())
-lst=[0]
-dp=[0]*(floor*(floor+1)//2+1)
+'''
+dp 지나가면서 행별로 max값 두 개씩 정해야함
+'''
 
-for i in range(floor):
-    lst.extend(list(map(int, sys.stdin.readline().split())))
+t = int(sys.stdin.readline())
 
-# i=1
-# for cur in range(floor+1):
-#     for j in range(cur):
-#         print(lst[i+j], end=' ')
-#     i+=cur
-#     print()
+for _ in range(t):
+    max_=0
+    n = int(sys.stdin.readline())
+    lst=[]
+    dp=[[0]*n for i in range(2)]
+    lst.append(list(map(int, sys.stdin.readline().split())))
+    lst.append(list(map(int, sys.stdin.readline().split())))
+    
+    if n == 1:
+        print(max(lst[0][0], lst[1][0]))
+    elif n == 2:
+        print(max(lst[0][1]+lst[1][0], lst[0][0]+lst[1][1]))
+    else:   
+        dp[0][0] = lst[0][0]
+        dp[1][0] = lst[1][0]
 
+        dp[0][1] = lst[1][0] + lst[0][1]
+        dp[1][1] = lst[0][0] + lst[1][1]
+        
+        # 각 행의 max 2개씩 : 0행 계산용, 1행 계산용
+        max_list = [[dp[0][0],dp[0][1]],
+                    [dp[1][0],dp[1][1]]]
+        # 0행 계산 시: 0번과 3번 비교
+        # 1행 계산 시: 1번과 2번 비교
+        
+        for i in range(2, n):
+            dp[0][i] = lst[0][i] + max(max_list[0][0], max_list[1][0], max_list[1][1])
+            dp[1][i] = lst[1][i] + max(max_list[0][0], max_list[0][1], max_list[1][0])
 
-# i = 각 층의 가장 첫 번째 수
-# 이전 층의 수만을 고르는게 필요
-i=2
-prev_i=0
-dp[1]=lst[1]
-max_ = lst[1]
-for cur in range(2, floor+1):
-    # print('cur:',cur)
-    for j in range(cur):
-        # print(lst[i+j],end=' ')
-        left = i+j-cur
-        right = i+j-cur+1
-        if left < prev_i:
-            left = 0
-        if right >= i:
-            right = 0
-        # print(lst[left], lst[right])
-        dp[i+j] = lst[i+j]+max(dp[left], dp[right])
-        max_ = max(max_, dp[i+j])
-    prev_i=i
+            max_list = [[max(max_list[0][0], max_list[0][1]), dp[0][i]], 
+                        [max(max_list[1][0], max_list[1][1]), dp[1][i]]]
 
-    i+=cur
-#     print()
-
-# print(lst)
-# print(dp)
-print(max_)
+            max_ = max(dp[0][i], dp[1][i], max_)
+        
+        print(max_)
+    # print()
+    # for l in lst:
+    #     print(l)
+    # print()
+    # for d in dp:
+    #     print(d)
+    # print()
