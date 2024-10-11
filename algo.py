2177ae35563711651a224d1e5ced6a66e96869f2
hobbang <hocheol0303@gmail.com>
2024-10-11 09:03:29 +0900

    14502_개쩌는 백트래킹 문제

--- algo.py
+++ algo.py
@@ -1,26 +1,65 @@
 '''
-변수 범위 보라 시간초과가 날 수밖에 없다 (1초 - 1024*1024*100000)
-DP(점화식)로 풀어야 한다 -> 어케했노;;
-먼저 dp 테이블을 (0,0)에서 현위치까지의 합으로 채워
-행렬 그림 그리면서 dp 테이블을 어떻게 활용할지 생각
-컨닝 좋은 블로그: https://jominseoo.tistory.com/101
+감도 안잡힘
+bfs
+벽을 세우는 백트래킹
+백트래킹으로 벽 세우고 3개에서 bfs로 바이러스 퍼뜨리고 0 개수 확인 -> 개수 max 찾기
+
+약간 브루트포스향 나는 백트래킹
 '''
 
 import sys
+from collections import deque
+import copy
+
+def virus():
+    global n, m, lst, result
+    q = deque()
+    d_row = [-1, 1, 0, 0]
+    d_col = [0, 0, -1, 1]
+
+    n_lst = copy.deepcopy(lst)
+    
+    for i in range(n):
+        for j in range(m):
+            if n_lst[i][j] == 2:
+                q.append((i, j))
+
+    while q:
+        row, col = q.popleft()
+        for i in range(4):
+            n_row = row + d_row[i]
+            n_col = col + d_col[i]
+            if n_row >= n or n_row < 0 or n_col >= m or n_col < 0: continue
+            elif n_lst[n_row][n_col] == 0:
+                n_lst[n_row][n_col] = 2
+                q.append((n_row, n_col))
+    
+    count = 0
+
+    for i in range(n):
+        count+=n_lst[i].count(0)
+
+    result = max(result, count)
+
+
+# 백트래킹 함수
+def dfs(cnt):
+    global lst
+    if cnt == 3:
+        virus()
+        return
+    for i in range(n):
+        for j in range(m):
+            if lst[i][j] == 0:
+                lst[i][j] = 1
+                dfs(cnt+1)
+                lst[i][j] = 0
 
 n, m = map(int, sys.stdin.readline().split())
-lst=[]
-dp = [[0]*(n+1) for i in range(n+1)]
-for i in range(n):
+lst = []
+result = 0
+for _ in range(n):
     lst.append(list(map(int, sys.stdin.readline().split())))
 
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        dp[i][j] = dp[i-1][j] + dp[i][j-1] + lst[i-1][j-1] - dp[i-1][j-1]
-
-for _ in range(m):
-    result=0
-    r1, c1, r2, c2 = map(int, sys.stdin.readline().split())
-
-    result = dp[r2][c2] - dp[r2][c1-1] - dp[r1-1][c2] + dp[r1-1][c1-1]
-    print(result)
+dfs(0)
+print(result)
