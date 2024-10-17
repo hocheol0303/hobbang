a70eafd0af52dccb3c0c475766e8185f8c9b5f68
hobbang <hocheol0303@gmail.com>
2024-10-18 08:58:57 +0900

    1238_dijkstra_GOAT

--- algo.py
+++ algo.py
@@ -1,19 +1,58 @@
-import sys
+'''
+n, m, x : 학생 수, 길 수, 목적지
 
-def dfs(start):
-    global n, m, lst
-    if len(lst) == m:
-        print(*lst)
-    else:
-        for i in range(1, n+1):
-            if i in lst:
+컨닝
+'''
+
+import sys
+import heapq
+
+def dijkstra(start):
+    global x, costs
+    s = start
+    heap = []
+    heapq.heappush(heap, (0, start))
+    costs = [float('inf')]*(n+1)
+    
+    while heap:
+        cost, start = heapq.heappop(heap)
+        if costs[start] != float('inf'):
+            continue
+        else:
+            costs[start] = cost
+            if costs[start] < cost:
                 continue
             else:
-                lst.append(i)
-                dfs(i)
-                lst.pop()
+                for n_cost, dest in grp[start]:
+                    if costs[dest] > cost+n_cost:
+                        heapq.heappush(heap, (cost+n_cost, dest))
+    
+    if s == x:
+        return costs
+    return costs[x]
 
-n, m = map(int, sys.stdin.readline().split())
-lst = []
+n, m, x = map(int, sys.stdin.readline().split())
+grp = [[] for _ in range(n+1)]
 
-dfs(0)
+come_cost = None
+go_cost = [float('inf')]*(n+1)
+
+for _ in range(m):
+    start, dest, cost = map(int, sys.stdin.readline().split())
+    grp[start].append((cost, dest))
+
+for i in range(1, n+1):
+    if i == x:
+        come_cost = dijkstra(x)
+    else:
+        go_cost[i] = dijkstra(i)
+
+max_ = 0
+for i in range(1, n+1):
+    if go_cost[i]+come_cost[i] == float('inf'):
+        continue
+    else:
+        max_ = max(max_, go_cost[i]+come_cost[i])
+
+# print(go_cost, come_cost)
+print(max_)
