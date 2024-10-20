4d6ef02cdd5f2f1747b5312c35a83ee37a4bf880
hobbang <hocheol0303@gmail.com>
2024-10-20 11:22:43 +0900

    7562_bfs

--- algo.py
+++ algo.py
@@ -1,30 +1,41 @@
 '''
-맨 앞 10은 고정
-(0으로 끝나는거 개수 * 2) + (1로 끝나는거 개수)
-
-n = 1: 1
-    0으로 끝나는거 0, 1로 끝나는거 1
-n = 2: 10
-    0으로 끝나는거 1, 1로 끝나는거 0 : 0에서 파생 0, 1에서 파생 1
-n = 3: 100, 101
-    0으로 끝나는거 1, 1로 끝나는거 1 : 0에서 파생 2, 1에서 파생 0
-n = 4: 1000, 1001, 1010
-    0으로 끝나는거 2, 1로 끝나는거 1 : 0에서 파생 2, 1에서 파생 1
-n = 5: 10000, 10001, 10010, 10100, 10101
-    0으로 끝나는거 3, 1로 끝나는거 2 : 0에서 파생 4, 1에서 파생 1
-
-n = k에서의 규칙
-    k-1에서 0의 개수만큼 0, 1의 개수만큼 0, 0의 개수만큼 1
+12시 방향부터 시계
+d_row = [-2, -1, 1, 2, 2, 1, -1, -2]
+d_col = [1, 2, 2, 1, -1, -2, -2, -1]
 '''
 
 import sys
-n = int(sys.stdin.readline())
-zeros = [0]
-ones = [1]
+from collections import deque
 
+def bfs(l, start_row, start_col, dest_row, dest_col):
+    global lst
+    d_row = [-2, -1, 1, 2, 2, 1, -1, -2]
+    d_col = [1, 2, 2, 1, -1, -2, -2, -1]
+    q = deque()
+    q.append((start_row, start_col))
 
-for i in range(1, n):
-    zeros.append(zeros[-1] + ones[-1])
-    ones.append(zeros[-2])
+    while q:
+        row, col = q.popleft()
+        
+        for i in range(8):
+            n_row, n_col = row+d_row[i], col+d_col[i]
+            if 0 <= n_row < l and 0 <= n_col < l:
+                if lst[n_row][n_col] == 0:
+                    lst[n_row][n_col] = lst[row][col]+1
+                    q.append((n_row, n_col))
 
-print(zeros[-1] + ones[-1])
+        if lst[dest_row][dest_col] != 0:
+            print(lst[dest_row][dest_col])
+            break
+
+tc = int(sys.stdin.readline())
+for _ in range(tc):
+    l = int(sys.stdin.readline())
+    lst = [[0]*l for i in range(l)]
+
+    start = tuple(map(int, sys.stdin.readline().split()))
+    dest = tuple(map(int, sys.stdin.readline().split()))
+    if start == dest:
+        print(0)
+    else:
+        bfs(l, *start, *dest)
