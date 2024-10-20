723750284b5f24245eeb111f2c20fbac0f58dd6a
hobbang <hocheol0303@gmail.com>
2024-10-21 08:17:36 +0900

    12851

--- algo.py
+++ algo.py
@@ -1,41 +1,49 @@
 '''
-12시 방향부터 시계
-d_row = [-2, -1, 1, 2, 2, 1, -1, -2]
-d_col = [1, 2, 2, 1, -1, -2, -2, -1]
+최단시간, 최단시간 개수
+x+1, x-1, 2*x로 이동
+
+3 0에서 3 1이 뜸 -> dp[n+1] > dp[n]+1 if문을 dp[n+1] >= dp[n]+1로 바꿈
+0 0일 때 정답: 0 1, 내 답: 0 0
+들여쓰기 이녀석아
 '''
 
 import sys
 from collections import deque
 
-def bfs(l, start_row, start_col, dest_row, dest_col):
-    global lst
-    d_row = [-2, -1, 1, 2, 2, 1, -1, -2]
-    d_col = [1, 2, 2, 1, -1, -2, -2, -1]
-    q = deque()
-    q.append((start_row, start_col))
+n, k = map(int, sys.stdin.readline().split())
+dp = [float('inf')]*100001
+count = [0]*100001
+q = deque()
 
-    while q:
-        row, col = q.popleft()
-        
-        for i in range(8):
-            n_row, n_col = row+d_row[i], col+d_col[i]
-            if 0 <= n_row < l and 0 <= n_col < l:
-                if lst[n_row][n_col] == 0:
-                    lst[n_row][n_col] = lst[row][col]+1
-                    q.append((n_row, n_col))
+q.append(n)
+dp[n] = 0
 
-        if lst[dest_row][dest_col] != 0:
-            print(lst[dest_row][dest_col])
-            break
+while q:
+    n = q.popleft()
 
-tc = int(sys.stdin.readline())
-for _ in range(tc):
-    l = int(sys.stdin.readline())
-    lst = [[0]*l for i in range(l)]
+    if n+1 <= 100000:
+        if dp[n+1] >= dp[n]+1:
+            dp[n+1] = dp[n]+1
+            q.append(n+1)
+    if n-1 >= 0:
+        if dp[n-1] >= dp[n]+1:
+            dp[n-1] = dp[n]+1
+            q.append(n-1)
+    if n*2 <= 100000:
+        if dp[n*2] >= dp[n]+1:
+            dp[n*2] = dp[n]+1
+            q.append(n*2)
+    
+    if n == k:
+        count[dp[k]] += 1
 
-    start = tuple(map(int, sys.stdin.readline().split()))
-    dest = tuple(map(int, sys.stdin.readline().split()))
-    if start == dest:
-        print(0)
-    else:
-        bfs(l, *start, *dest)
+    # if dp[n]+1 <= 100000:
+        # if n+1 == k:
+        #     count[dp[n]+1] += 1
+        # if n-1 == k:
+        #     count[dp[n]+1] += 1
+        # if n*2 == k:
+        #     count[dp[n]+1] += 1
+
+print(dp[k])
+print(count[dp[k]])
