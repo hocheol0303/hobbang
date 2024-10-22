1b09735755528bbc9041240cec1cb707374e818d
hobbang <hocheol0303@gmail.com>
2024-10-23 08:25:49 +0900

    5567

--- algo.py
+++ algo.py
@@ -1,38 +1,37 @@
 '''
-아이디어 컨닝함: 2차원 DP행렬
-
-위에꺼 따라가다가 같은글자 만나면 1 올리고 뒤로 쭉쭉 -> 점화식
-
-ACAYKP
-CAPCAK
-
-  A C A Y K P
-C 0 1 1 1 1 1
-A 1 1 2 2 2 2
-P 1 1 2 2 2 3
-C 1 1 2 2 2 3
-A 1 2 3 3 3 3
-K 1 2 3 3 4 4
-
-DP 테이블: 0 패딩 만들어쓰자
-같은 문자열 나오면 dp[i-1][j-1] + 1
+depth를 정해줘야겠다
+친구의 친구까지만
 '''
-
 import sys
+from collections import deque
 
-str1 = sys.stdin.readline().rstrip()
-str2 = sys.stdin.readline().rstrip()
+def bfs(start):
+    global n, m, dct, visited
+    q = deque()
+    q.append((start, 0))
+    visited[start] = True
 
-dp = [[0]*(len(str2)+1) for _ in range(len(str1)+1)]
-
-for i in range(1, len(str1)+1):
-    for j in range(1, len(str2)+1):
-        if str1[i-1] == str2[j-1]:
-            dp[i][j] = dp[i-1][j-1] + 1
+    while q:
+        start, depth = q.popleft()
+        if depth > 1:
+            continue
         else:
-            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+            for i in dct[start]:
+                if visited[i] == False:
+                    visited[i] = True
+                    q.append((i, depth+1))
 
-# for i in dp:
-#     print(i)
+n = int(sys.stdin.readline())
+m = int(sys.stdin.readline())
+dct={i:[] for i in range(1, n+1)}
+visited=[False]*(n+1)
+for _ in range(m):
+    start, end = map(int, sys.stdin.readline().split())
+    if end not in dct[start]:
+        dct[start].append(end)
+        dct[end].append(start)
 
-print(dp[-1][-1])
+bfs(1)
+
+# print(dct)
+print(visited.count(True)-1)
