189aa2925ac43b7f5397289ae280863b1f715c17
hobbang <hocheol0303@gmail.com>
2024-10-24 08:33:33 +0900

    1912

--- algo.py
+++ algo.py
@@ -1,37 +1,20 @@
 '''
-depth를 정해줘야겠다
-친구의 친구까지만
+연속된 몇 개의 수
 '''
+
 import sys
-from collections import deque
-
-def bfs(start):
-    global n, m, dct, visited
-    q = deque()
-    q.append((start, 0))
-    visited[start] = True
-
-    while q:
-        start, depth = q.popleft()
-        if depth > 1:
-            continue
-        else:
-            for i in dct[start]:
-                if visited[i] == False:
-                    visited[i] = True
-                    q.append((i, depth+1))
 
 n = int(sys.stdin.readline())
-m = int(sys.stdin.readline())
-dct={i:[] for i in range(1, n+1)}
-visited=[False]*(n+1)
-for _ in range(m):
-    start, end = map(int, sys.stdin.readline().split())
-    if end not in dct[start]:
-        dct[start].append(end)
-        dct[end].append(start)
+max_ = -float('inf')
 
-bfs(1)
+lst = [0]
+lst.extend(list(map(int, sys.stdin.readline().split())))
+dp = [0]*(n+1)
 
-# print(dct)
-print(visited.count(True)-1)
+for i in range(1, n+1):
+    dp[i] = max(lst[i]+dp[i-1], lst[i])
+    max_ = max(max_, dp[i])
+    
+# print(dp)
+# print(lst)
+print(max_)
