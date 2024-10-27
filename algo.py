0fcf097c9ce33735d0027927837ef49c0c6c3f6e
hobbang <hocheol0303@gmail.com>
2024-10-28 07:35:36 +0900

    1261_hardBFS

--- algo.py
+++ algo.py
@@ -1,19 +1,48 @@
+'''
+컨닝: 0 먼저 돌기(appendleft)
+'''
+
 import sys
 from collections import deque
 
-n, k = map(int, sys.stdin.readline().split())
-q = deque()
-count=0
+def bfs(start):
+    global count, lst, n, m
+    row, col = start
+    d_row = [-1, 1, 0, 0]
+    d_col = [0, 0, -1, 1]
+    q = deque()
+    q.append((row, col))
+
+
+    while q:
+        row, col = q.popleft()
+
+        for i in range(4):
+            n_row, n_col = row+d_row[i], col+d_col[i]
+            if 0 <= n_row < m and 0 <= n_col < n:
+                if lst[n_row][n_col] == 0:
+                    if count[n_row][n_col] > count[row][col]:
+                        count[n_row][n_col] = count[row][col]
+                        q.appendleft((n_row, n_col))
+                else:
+                    if count[n_row][n_col] > count[row][col]+1:
+                        q.append((n_row, n_col))
+                        count[n_row][n_col] = count[row][col]+1
+
+n, m = map(int, sys.stdin.readline().split())
 lst=[]
+count=[[float('inf')]*n for _ in range(m)]
 
-for i in range(1, n+1):
-    q.append(i)
+for _ in range(m):
+    lst.append(list(map(int, list(sys.stdin.readline().rstrip()))))
 
-while q:
-    count+=1
-    if count % k == 0:
-        lst.append(q.popleft())
-    else:
-        q.append(q.popleft())
+count[0][0] = 0
+bfs((0,0))
 
-print('<'+', '.join(map(str, lst)), end='>')
+# for i in lst:
+#     print(i)
+# print()
+# for i in count:
+#     print(i)
+
+print(count[-1][-1])
