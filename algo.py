304198f00cbb8168d71b5992c544b27c206da845
hobbang <hocheol0303@gmail.com>
2024-11-04 08:31:16 +0900

    11055

--- algo.py
+++ algo.py
@@ -1,42 +1,24 @@
 '''
-바ㄴ례
-4 3
-1 2
-2 3
-1 3
-2 다음 3 들을 수 있으므로 1 2 3 1 이 정답이래
-조건으로 넣어줬으면 좋았을걸
-
-바꿔야해
+1. 끝 값 처리: n = 1일 때
+2. 조건이 맞는데 더 가까이에 조건 맞는게 있을 때
+    5
+    1 8 2 3 9
 '''
-
 import sys
-from collections import deque
 
-def bfs(dest):
-    global lst, grp
-    q = deque()
-    q.append(dest)
+n = int(sys.stdin.readline())
+lst = list(map(int, sys.stdin.readline().split()))
+dp = [0]*n
+dp[0] = lst[0]
+max_ = lst[0]
 
-    if grp[dest] == []:
-        lst[dest] = 1
+for i in range(1, n):
+    for j in range(i-1, -1, -1):
+        if lst[j] < lst[i]:
+            dp[i] = max(lst[i] + dp[j], dp[i])
+    if dp[i] == 0:
+        dp[i] = lst[i]
+    max_ = max(max_, dp[i])
 
-    while q:
-        dest = q.popleft()
-        for i in grp[dest]:
-            lst[dest] = max(lst[dest], lst[i]+1)
-
-n, m = map(int, sys.stdin.readline().split())
-grp = {i:[] for i in range(1, n+1)}
-lst = [0] * (n+1)
-
-for _ in range(m):
-    start, end = map(int, sys.stdin.readline().split())
-    grp[end].append(start)
-
-for i in range(1, n+1):
-    if lst[i] == 0:
-        bfs(i)
-    
-print(*lst[1:])
-# print(grp)
+# print(dp)
+print(max_)
