a8265903b402b01bb111579aa9fa90c2a5aad12e
hobbang <hocheol0303@gmail.com>
2024-11-03 19:07:56 +0900

    14567

--- algo.py
+++ algo.py
@@ -1,21 +1,42 @@
-import sys
-n = int(sys.stdin.readline())
+'''
+바ㄴ례
+4 3
+1 2
+2 3
+1 3
+2 다음 3 들을 수 있으므로 1 2 3 1 이 정답이래
+조건으로 넣어줬으면 좋았을걸
 
-lst = [0]*(n+1)
-dp = [0]*(n+1)
+바꿔야해
+'''
+
+import sys
+from collections import deque
+
+def bfs(dest):
+    global lst, grp
+    q = deque()
+    q.append(dest)
+
+    if grp[dest] == []:
+        lst[dest] = 1
+
+    while q:
+        dest = q.popleft()
+        for i in grp[dest]:
+            lst[dest] = max(lst[dest], lst[i]+1)
+
+n, m = map(int, sys.stdin.readline().split())
+grp = {i:[] for i in range(1, n+1)}
+lst = [0] * (n+1)
+
+for _ in range(m):
+    start, end = map(int, sys.stdin.readline().split())
+    grp[end].append(start)
 
 for i in range(1, n+1):
-    lst[i] = int(sys.stdin.readline())
-
-if n > 2:
-    dp[1] = lst[1]
-    dp[2] = dp[1]+lst[2]
-
-    for i in range(3, n+1):
-        dp[i] = max(dp[i-1], lst[i]+dp[i-2], lst[i]+lst[i-1]+dp[i-3])
+    if lst[i] == 0:
+        bfs(i)
     
-    # print(lst)
-    # print(dp)
-    print(dp[-1])
-else:
-    print(sum(lst))
+print(*lst[1:])
+# print(grp)
