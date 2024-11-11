251a74f2b6404ce700f5b1980af586d4df13786c
hobbang <hocheol0303@gmail.com>
2024-11-11 09:29:01 +0900

    4963

--- algo.py
+++ algo.py
@@ -1,12 +1,35 @@
 import sys
-n = int(sys.stdin.readline())
+from collections import deque
 
-lst = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-dp = [0]*(n+1)
+def bfs(row, col):
+    global lst, w, h
+    d_row = [-1, -1, 0, 1, 1, 1, 0, -1]
+    d_col = [0, 1, 1, 1, 0, -1, -1, -1]
+    q = deque()
+    q.append((row, col))
+    lst[row][col] = '.'
 
-for i in range(n):
-    for j in range(i+lst[i][0], n+1):
-        dp[j] = max(dp[j], dp[i]+lst[i][1])
+    while q:
+        row, col = q.popleft()
+        for i in range(8):
+            n_row, n_col = row+d_row[i], col+d_col[i]
+            if 0 <= n_row < h and 0 <= n_col < w and lst[n_row][n_col] == 1:
+                lst[n_row][n_col] = '.'
+                q.append((n_row, n_col))
 
-# print(lst)
-print(dp[-1])
+
+while True:
+    w, h = map(int, sys.stdin.readline().split())
+    if w == 0 and h == 0:
+        break
+    else:
+        count = 0
+        lst = []
+        for _ in range(h):
+            lst.append(list(map(int, sys.stdin.readline().split())))
+        for r in range(h):
+            for c in range(w):
+                if lst[r][c] == 1:
+                    count += 1
+                    bfs(r, c)
+        print(count)
