3336241c126238a8e807d85ddbd81dd04e6dc068
hobbang <hocheol0303@gmail.com>
2024-11-15 14:09:38 +0900

    15651

--- algorithm/algo.py
+++ algorithm/algo.py
@@ -1,39 +1,16 @@
 import sys
-from collections import deque
 
-def bfs(start):
-    global grp, distances
-    q = deque()
+def dfs(start):
+    global m, lst
+    if len(lst) == m:
+        print(*lst)
+    else:
+        for i in range(1, n+1):
+            lst.append(i)
+            dfs(i+1)
+            lst.pop()
 
-    q.append(start)
-    distances[start] = 0
+n, m = map(int, sys.stdin.readline().split())
+lst = []
 
-    while q:
-        start = q.popleft()
-        for end in grp[start]:
-            if distances[end] > distances[start]+1:
-                distances[end] = distances[start]+1
-                q.append(end)
-
-n, m, k, x = map(int, sys.stdin.readline().split())
-
-grp = {i:[] for i in range(1, n+1)}
-distances = [float('inf')] * (n+1)
-
-for _ in range(m):
-    start, end = map(int, sys.stdin.readline().split())
-    grp[start].append(end)
-
-# print(grp)
-bfs(x)
-# print(distances)
-ans=[]
-for i in range(1, n+1):
-    if distances[i] == k:
-        ans.append(i)
-
-if len(ans) == 0:
-    print(-1)
-else:
-    for i in sorted(ans):
-        print(i)
+dfs(1)
